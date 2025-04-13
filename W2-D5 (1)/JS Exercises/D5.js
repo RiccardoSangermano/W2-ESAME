aa798ae93c98646f842a0ce3b1cabed125c21d91
RiccardoSangermano <riccardosangermano297@gmail.com>
2025-04-13 15:33:29 +0200

    final

--- W2-D5 (1)/JS Exercises/D5.js
+++ W2-D5 (1)/JS Exercises/D5.js
@@ -1,42 +1,31 @@
 /*
-REGOLE
-- Tutte le risposte devono essere scritte in JavaScript
-- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
-- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
-- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
-- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
-*/
-
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
-for (let i = 0; 0 < pets.length; i++) {
-  let animals = pets[i]
+const pets = ['dog', 'cat', 'hamster', 'redfish'];
+for (let i = 0; i < pets.length; i++) {
+  console.log(pets[i])
 }
-console.log(animals)
-
-
-
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
-
 */
-let animals = ["dog","cat", "hamster", "redfish"]
-console.log(animals.sort())
+const pets2 = ["dog", "cat", "hamster", "redfish"]  
+console.log(pets2.sort())
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
-console.log(animals.reverse())
+const pets3 = ["dog", "cat", "hamster", "redfish"]; // Ho creato una copia per evitare modifiche all'array originale
+console.log(pets3.reverse())
 
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
-let domestic = pets.shift()
-pets.push(domestic)
-console.log(pets)
+const pets4 = ['dog', 'cat', 'hamster', 'redfish']
+let primoElemento = pets4.shift()
+pets4.push(primoElemento)
+console.log(pets4)
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -60,10 +49,9 @@ const cars = [
     color: 'black',
     trims: ['life', 'style', 'r-line'],
   },
-]
-let licensePlate = "Patente"
-for (let i=0; i < cars.length; i++){
-  cars[i].licensePlate= licensePlate
+];
+for (let i = 0; i < cars.length; i++) {
+  cars[i].licensePlate = 'ABC1234'
 }
 console.log(cars)
 
@@ -71,94 +59,15 @@ console.log(cars)
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
-const newType = {
-  brand: "Ferrari",
-  model: "655f",
-  color: "rosso",
-  trims: ["confort", "plus"],
+const newCar = {
+  brand: 'Ferrari',
+  model: '655f',
+  color: 'rosso',
+  trims: ['confort', 'plus'],
 }
-cars.push(newType)
-for (let i = 0; i < cars.length; i++){
-  cars[i].trims,pop()
+cars.push(newCar)
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop()
 }
 console.log(cars)
-
-/* ESERCIZIO 7
-    Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
-*/
-
-
-/* ESERCIZIO 8
-    Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
-    "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
-*/
-
-/* ESERCIZIO 9
-    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
-*/
-const numericArray = [
-  6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
-]
-
-let i=0;
-while(i<numericArray.length){
-  console.log(numericArray[i])
-  i++;
-}
-
-
-
-
-
-
-
-/* ESERCIZIO 10
-    Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
-    dell'alfabeto italiano.
-    es. [f, b, e] --> [6, 2, 5]
-*/
-const charactersArray = ['g', 'n', 'u', 'z', 'd']
-const numberAlphabeth=[
-  a = 1,
-  b = 2,
-  c = 3,
-  d = 4,
-  e = 5,
-  f = 6,
-  g = 7,
-  h = 8,
-  i = 9,
-  l =10,
-  m = 11,
-  n = 12,
-  o = 13,
-  p = 14,
-  q = 16,
-  r = 17,
-  s = 18,
-  t = 19,
-  u = 20,
-  v = 21,
-  z = 22,
-]
-switch(numberAlphabeth){
-  case "g":
-  console.log("La lettera g equivale al numero 7")
-  break;
-  case "n":
-    console.log("La lettera n equivale al numero 12")
-    break;
-    case "u":
-    console.log("La lettera u equivale al numero 20")
-    break;
-    case "z":
-      console.log("La lettera z equivale al numero 22")
-      case "d":
-        console.log("La lettera d equivale al numero 4")
-        break;
-        default:
-          console.log("Non ho trovato corrispondenze")
-}
-
-
 
